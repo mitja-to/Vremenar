af61e65d4058028662be2fd90872c137d14242ab
mitja-to <toplak.mitja@gmail.com>
2015-11-17 17:38:45 +0100

    Dodana nova layouta za seznam (RecyclerView in CardView) ter layout za dodajanje novega mesta

---
+++ app/src/main/java/com/vremenar/activity_dodaj.java
@@ -0,0 +1,16 @@
+package com.vremenar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mitja on 17. 11. 2015.
+ */
+public class activity_dodaj extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dodaj);
+    }
+}
